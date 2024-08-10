79705dd77c07b306786d31242abb2ee4e58f2505
unknown <jagdeeshbhavani22@gmail.com>
2024-08-10 22:40:55 +0530

    feat:header completed

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MealPages() {
+  return (
+    <main>
+      <div>page</div>
+    </main>
+  );
+}
+
+export default MealPages;
---
+++ app/meals/page.js
@@ -0,0 +1,7 @@
+export default function MealsPage() {
+  return (
+    <main>
+      <p>Hello....</p>
+    </main>
+  );
+}
