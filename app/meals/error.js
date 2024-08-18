25595b657c01ec1ff8a5d4b03fb820127b358cee
unknown <jagdeeshbhavani22@gmail.com>
2024-08-19 00:24:05 +0530

    feat:error component added

---
+++ app/meals/error.js
@@ -0,0 +1,9 @@
+"use client";
+export default function Error() {
+  return (
+    <main className="error">
+      <h1>Error Occured</h1>
+      <p>Please contact your adminstrator</p>
+    </main>
+  );
+}
