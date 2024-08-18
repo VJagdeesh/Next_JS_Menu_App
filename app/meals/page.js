2888b3d81939657407290bc0887b1c46e30aebc3
unknown <jagdeeshbhavani22@gmail.com>
2024-08-19 00:00:45 +0530

    feat:meals page completed

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,24 @@
-export default function MealsPage() {
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+export default async function MealsPage() {
+  const meals = await getMeals();
   return (
-    <main>
-      <p>Hello....</p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious Meals, Pages
+          <span className={classes.highlight}>By You</span>
+        </h1>
+        <p>Choose your meal and cook it yourself</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={meals} />
+      </main>
+    </>
   );
 }
