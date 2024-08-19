87568919185c557847fe60eb9512205be8ed72ee
unknown <jagdeeshbhavani22@gmail.com>
2024-08-19 10:47:07 +0530

    feat:indvidual meal page added & route not-found page added

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,10 +1,33 @@
 import React from "react";
-
-function MealPages() {
+import classes from "./page.module.css";
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
+function MealPages({ params }) {
+  const meal = getMeal(params.mealSlug);
+  meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
   return (
-    <main>
-      <div>page</div>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${`meal.creator_email`}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
   );
 }
 
