13892eca46fc43de9ed368236f9c8c4e8827440f
unknown <jagdeeshbhavani22@gmail.com>
2024-08-20 16:19:15 +0530

    feat:skeleton loading implemented

--- app/meals/loading-out.js
+++ app/meals/loading-out.js
@@ -1,4 +1,17 @@
 import classes from "./loading.module.css";
+import Skeleton from "react-loading-skeleton";
 export default function MealsLoading() {
-  return <p className={classes.loading}>Loading...</p>;
+  const skeletonItems = Array(6).fill(null);
+  return (
+    <main>
+      <div className={classes.skeletonGrid}>
+        {skeletonItems.map((_, index) => (
+          <div key={index} className={classes.skeletonItem}>
+            <Skeleton height={140} width={200} />
+            <Skeleton height={20} width={180} style={{ marginTop: "10px" }} />
+          </div>
+        ))}
+      </div>
+    </main>
+  );
 }
